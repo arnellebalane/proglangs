92fce28fce46ca93d87c9f77807708d972f99c61
Arnelle Balane <arnellebalane@gmail.com>
2015-02-26 11:33:09 +0800

    implemented postfix calculator

---
+++ postfix-calculator/postfix-calculator.js
@@ -0,0 +1,49 @@
+(function(root, definition) {
+    if (typeof define === 'function' && define.amd) {
+        define([], definition);
+    } else if (typeof exports === 'object') {
+        module.exports = definition();
+    } else {
+        root.calculate_postfix = definition();
+    }
+})(this, function() {
+    function evaluate(a, operator, b) {
+        if (operator === '+') {
+            return a + b;
+        } else if (operator === '-') {
+            return a - b;
+        } else if (operator === '*') {
+            return a * b;
+        } else if (operator === '/') {
+            return a / b;
+        }
+        throw new Error('Invalid Operation.');
+    }
+
+
+    function calculate(postfix) {
+        var stack = [];
+
+        postfix.forEach(function(token) {
+            if (token.type === 'operand') {
+                stack.push(token);
+            } else if (token.type === 'operator') {
+                var b = stack.pop();
+                var a = stack.pop();
+                if (a.type !== 'operand' || b.type !== 'operand') {
+                    throw new Error('Invalid Postfix Statement.');
+                }
+                var result = evaluate(a.value, token.value, b.value);
+                stack.push({ value: result, type: 'operand' });
+            }
+        });
+
+        if (stack.length > 1) {
+            throw new Error('Invalid Postfix Statement.');
+        }
+        return stack.pop().value;
+    }
+
+
+    return calculate;
+});
