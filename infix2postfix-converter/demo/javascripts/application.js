5ca15baa4e011f068421c44524f6bbad3d200532
Arnelle Balane <arnellebalane@gmail.com>
2015-03-02 07:39:29 +0800

    updated demo files to give instructions

--- infix2postfix-converter/demo/javascripts/application.js
+++ infix2postfix-converter/demo/javascripts/application.js
@@ -21,3 +21,29 @@ input.on('keydown keyup change', function(e) {
         output.text(e.message);
     }
 });
+
+
+setTimeout(function() {
+    input.val('ENTER');
+    container.width(5 * 20 + 38);
+}, 500);
+
+setTimeout(function() {
+    input.val('INFIX');
+    container.width(5 * 20 + 38);
+}, 1000);
+
+setTimeout(function() {
+    input.val('EQUATION');
+    container.width(8 * 20 + 38);
+}, 1500);
+
+setTimeout(function() {
+    input.val('NOW');
+    container.width(3 * 20 + 38);
+}, 2000);
+
+setTimeout(function() {
+    input.val('');
+    container.width(0 * 20 + 38);
+}, 2500);
--- postfix-calculator/demo/javascripts/application.js
+++ postfix-calculator/demo/javascripts/application.js
@@ -23,3 +23,29 @@ input.on('keydown keyup change', function(e) {
         }
     }
 });
+
+
+setTimeout(function() {
+    input.val('ENTER');
+    container.width(5 * 20 + 38);
+}, 500);
+
+setTimeout(function() {
+    input.val('POSTFIX');
+    container.width(7 * 20 + 38);
+}, 1000);
+
+setTimeout(function() {
+    input.val('EQUATION');
+    container.width(8 * 20 + 38);
+}, 1500);
+
+setTimeout(function() {
+    input.val('NOW');
+    container.width(3 * 20 + 38);
+}, 2000);
+
+setTimeout(function() {
+    input.val('');
+    container.width(0 * 20 + 38);
+}, 2500);
