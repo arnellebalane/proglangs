8cc00a3daa8d74ca27f214cbdb8ff836a2ffc363
Arnelle Balane <arnellebalane@gmail.com>
2015-03-15 02:14:23 +0800

    tokenize macros and functions

--- code-tokenizer/basic-code-tokenizer.js
+++ code-tokenizer/basic-code-tokenizer.js
@@ -8,9 +8,46 @@
         root.tokenize_code_basic = definition(root.tokenize_code);
     }
 })(this, function(_tokenize) {
+    function macros(tokens) {
+        var results = [];
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i].label === 'PREPROCESSOR_DIRECTIVE') {
+                var value = tokens[i].token;;
+                var end = i + 1;
+                var token = tokens[end];
+                while (token && !(token.label === 'WHITESPACE'
+                        && token.token === '\n')) {
+                    value += token.token;
+                    token = tokens[++end];
+                }
+                results.push(value);
+                i = end;
+            }
+        }
+        return results;
+    }
+
+
+    function functions(tokens) {
+        var results = [];
+        for (var i = 2; i < tokens.length; i++) {
+            if (tokens[i].label === 'IDENTIFIER' &&
+                    tokens[i - 2].label === 'DATATYPE'
+                    && (tokens[i + 1].label === 'OPENING_PARENTHESIS'
+                    || tokens[i + 2].label === 'OPENING_PARENTHESIS')) {
+                results.push(tokens[i].token);
+            }
+        }
+        return results;
+    }
+
+
     function tokenize(code) {
         var tokens = _tokenize(code);
-        return tokens;
+        return {
+            macros: macros(tokens),
+            functions: functions(tokens)
+        };
     }
 
 
