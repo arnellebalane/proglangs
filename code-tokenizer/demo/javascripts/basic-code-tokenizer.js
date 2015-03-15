07a2473b0b8e9246e22dd48c2691f02916491d68
Arnelle Balane <arnellebalane@gmail.com>
2015-03-16 02:27:24 +0800

    initial demo app for code tokenizer

---
+++ code-tokenizer/demo/javascripts/basic-code-tokenizer.js
@@ -0,0 +1,283 @@
+(function(root, definition) {
+    if (typeof define === 'function' && define.amd) {
+        define(['code-tokenizer'], definition);
+    } else if (typeof exports === 'object') {
+        var tokenize = require('./code-tokenizer');
+        module.exports = definition(tokenize);
+    } else {
+        root.tokenize_code_basic = definition(root.tokenize_code);
+    }
+})(this, function(_tokenize) {
+    function _macros(tokens) {
+        var results = [];
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i].label === 'PREPROCESSOR_DIRECTIVE') {
+                var value = tokens[i].token;;
+                var end = i + 1;
+                var token = tokens[end];
+                while (token && !(token.label === 'WHITESPACE'
+                        && token.token === '\n')) {
+                    value += token.token;
+                    token = tokens[++end];
+                }
+                results.push(value);
+                i = end;
+            }
+        }
+        return results;
+    }
+
+
+    function _functions(tokens) {
+        var results = [];
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i].label === 'IDENTIFIER'
+                    && tokens[i - 1].label === 'WHITESPACE'
+                    && tokens[i - 2].label === 'DATATYPE'
+                    && (tokens[i + 1].label === 'OPENING_PARENTHESIS'
+                    || tokens[i + 2].label === 'OPENING_PARENTHESIS')
+                    && results.indexOf(tokens[i].token) === -1) {
+                results.push(tokens[i].token);
+            }
+        }
+        return results;
+    }
+
+
+    function _function_calls(tokens) {
+        var results = [];
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i].label === 'IDENTIFIER'
+                    && tokens[i - 2]
+                    && tokens[i - 2].label !== 'DATATYPE'
+                    && (tokens[i + 1].label === 'OPENING_PARENTHESIS'
+                    || tokens[i + 2].label === 'OPENING_PARENTHESIS')
+                    && results.indexOf(tokens[i].token) === -1) {
+                results.push(tokens[i].token);
+            }
+        }
+        return results;
+    }
+
+
+    function _keywords(tokens) {
+        var results = [];
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i].label.match(/^(KEYWORD|DATATYPE)$/)
+                    && results.indexOf(tokens[i].token) === -1) {
+                results.push(tokens[i].token);
+            }
+        }
+        return results;
+    }
+
+
+    function _operators(tokens) {
+        var results = [];
+        var labels = ['OPERATION', 'COMPARISON', 'INCREMENT', 'DECREMENT',
+            'LOGICAL_OPERATION', 'OPERATION_ASSIGNMENT', 'ASSIGNMENT',
+            'NOT_OPERATION', 'DOT_OPERATION', 'TERNARY_OPERATION',
+            'TERNARY_SELECTION'];
+        for (var i = 0; i < tokens.length; i++) {
+            if (labels.indexOf(tokens[i].label) > -1
+                    && results.indexOf(tokens[i].token) === -1) {
+                results.push(tokens[i].token);
+            }
+        }
+        return results;
+    }
+
+
+    function _punctuators(tokens) {
+        var results = [];
+        var labels = ['OPENING_PARENTHESIS', 'CLOSING_PARENTHESIS',
+            'OPENING_ANGLE_BRACKET', 'CLOSING_ANGLE_BRACKET',
+            'OPENING_BRACKET', 'CLOSING_BRACKET', 'OPENING_BRACES',
+            'CLOSING_BRACES', 'DELIMITER', 'SINGLE_QUOTE', 'DOUBLE_QUOTE'];
+        for (var i = 0; i < tokens.length; i++) {
+            if (labels.indexOf(tokens[i].label) > -1
+                    && results.indexOf(tokens[i].token) === -1) {
+                results.push(tokens[i].token);
+            }
+        }
+        return results;
+    }
+
+
+    function _arguments(tokens) {
+        var results = [];
+        var calls = _function_calls(tokens);
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i].label === 'OPENING_PARENTHESIS'
+                    && (tokens[i - 1].label === 'IDENTIFIER'
+                    || tokens[i - 2].label === 'IDENTIFIER')) {
+                var function_name = tokens[i - 1].label === 'IDENTIFIER'
+                    ? tokens[i - 1].token : tokens[i - 2].token;
+                if (calls.indexOf(function_name) > -1) {
+                    var count = 1;
+                    var start = i + 1;
+                    for (var j = i + 1; count; j++) {
+                        if (tokens[j].label === 'OPENING_PARENTHESIS') {
+                            count++;
+                        } else if (tokens[j].label === 'CLOSING_PARENTHESIS') {
+                            count--;
+                            if (!count) {
+                                var value = '';
+                                while (start < j) {
+                                    value += tokens[start++].token;
+                                }
+                                if (value.length) {
+                                    results.push(value);
+                                }
+                            }
+                        } else if (tokens[j].label === 'DELIMITER') {
+                            if (count === 1) {
+                                var value = '';
+                                while (start < j) {
+                                    value += tokens[start++].token;
+                                }
+                                if (value.length) {
+                                    results.push(value);
+                                }
+                                start++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return results;
+    }
+
+
+    function _parameters(tokens) {
+        var results = [];
+        var funcs = _functions(tokens);
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i].label === 'IDENTIFIER'
+                    && funcs.indexOf(tokens[i].token) > -1) {
+                var start = i;
+                var end = i;
+                var value = '';
+                while (tokens[++start].label !== 'OPENING_PARENTHESIS');
+                while (tokens[++end].label !== 'CLOSING_PARENTHESIS');
+                while (++start < end) {
+                    if (tokens[start].label === 'DELIMITER') {
+                        results.push(value.trim());
+                        value = '';
+                    } else {
+                        value += tokens[start].token;
+                    }
+                }
+                if (value.trim()) {
+                    results.push(value.trim());
+                }
+            }
+        }
+        return results;
+    };
+
+
+    function _variables(tokens) {
+        var results = [];
+        var defined = _functions(tokens);
+        var calls = _function_calls(tokens);
+        var reserved = _keywords(tokens);
+        var comments = _comments(tokens, true);
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i].label === 'IDENTIFIER'
+                    && comments.indexOf(i) === -1
+                    && defined.indexOf(tokens[i].token) === -1
+                    && calls.indexOf(tokens[i].token) === -1
+                    && reserved.indexOf(tokens[i].token) === -1
+                    && tokens[i - 1].label.match(/^(WHITESPACE|DELIMITER)$/)
+                    && results.indexOf(tokens[i].token) === -1) {
+                results.push(tokens[i].token);
+            }
+        }
+        return results;
+    }
+
+
+    function _assignments(tokens) {
+        var results = [];
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i].label === 'ASSIGNMENT_OPERATION') {
+                var start = i;
+                var end = i;
+                var value = '';
+                while (tokens[--start].label !== 'IDENTIFIER');
+                while (tokens[++end].label !== 'DELIMITER');
+                while (start < end) {
+                    value += tokens[start++].token;
+                }
+                results.push(value);
+            }
+        }
+        return results;
+    }
+
+
+    function _comments(tokens, index) {
+        var results = [];
+        var indeces = [];
+        for (var i = 0, q = 1; i < tokens.length; i++) {
+            if (tokens[i].label === 'DOUBLE_QUOTE'
+                    && tokens[i - 1].token !== '\\') {
+                q *= -1;
+            } else if (tokens[i].token === '/'
+                    && tokens[i + 1].token === '/'
+                    && q === 1) {
+                var start = i;
+                var end = i;
+                var value = '';
+                while (tokens[++end].token !== '\n');
+                while (start < end) {
+                    indeces.push(start);
+                    value += tokens[start++].token;
+                }
+                results.push(value);
+                i = end - 1;
+            } else if (tokens[i].token === '/'
+                    && tokens[i + 1].token === '*'
+                    && q === 1) {
+                var start = i;
+                var end = i + 1;
+                var value = '';
+                while (!(tokens[end].token === '/'
+                        && tokens[end - 1].token === '*')
+                        && q === 1) {
+                    end++;
+                }
+                while (start <= end) {
+                    indeces.push(start);
+                    value += tokens[start++].token;
+                }
+                results.push(value);
+                i = end;
+            }
+        }
+        return index ? indeces : results;
+    };
+
+
+    function tokenize(code) {
+        var tokens = _tokenize(code);
+        return {
+            macros: _macros(tokens),
+            functions: _functions(tokens),
+            function_calls: _function_calls(tokens),
+            keywords: _keywords(tokens),
+            operators: _operators(tokens),
+            punctuators: _punctuators(tokens),
+            parameters: _parameters(tokens),
+            arguments: _arguments(tokens),
+            variables: _variables(tokens),
+            assignments: _assignments(tokens),
+            comments: _comments(tokens)
+        };
+    }
+
+
+    return tokenize;
+});
---
+++ code-tokenizer/demo/javascripts/code-tokenizer.js
@@ -0,0 +1,231 @@
+(function(root, definition) {
+    if (typeof define === 'function' && define.amd) {
+        define([], definition);
+    } else if (typeof exports === 'object') {
+        module.exports = definition();
+    } else {
+        root.tokenize_code = definition();
+    }
+})(this, function() {
+    function TokenizedCode(code) {
+        code = code.split('');
+        var tokens = [];
+        var index = 0;
+
+        while (index < code.length) {
+            var current = code[index];
+            if (this.letter(current)) {
+                var i = index;
+                while (this.alphanumeric(code[i])) {
+                    i++;
+                }
+                tokens.push(code.slice(index, i).join(''));
+                index = i - 1;
+            } else if (this.digit(current)) {
+                var i = index + 1;
+                index -= code[index - 1].match(/^[\+-]$/) ? 1 : 0;
+                while (this.number(code.slice(index, i).join(''))) {
+                    i++;
+                    if (code[i] === '.') {
+                        i += 2;
+                    }
+                }
+                tokens.push(code.slice(index, i - 1).join(''));
+                index = i - 2;
+            } else if (current.match(/^['"]$/)) {
+                var end = index + 1;
+                while (code[++end] !== current);
+                tokens.push(current, code.slice(index + 1, end).join(''));
+                tokens.push(current);
+                index = end;
+            } else if (this.whitespace(current)) {
+                if (tokens[tokens.length - 1] !== current) {
+                    tokens.push(current);
+                }
+            } else {
+                tokens.push(current);
+            }
+            index++;
+        }
+
+        this.tokens = [];
+        for (var i = 0; i < tokens.length; i++) {
+            var current = tokens[i];
+            if (current === '#') {
+                this._label(current, 'PREPROCESSOR_DIRECTIVE');
+            } else if (this.datatype(current)) {
+                var end = i;
+                while (this.datatype(tokens.slice(i, ++end).join(' ')));
+                this._label(tokens.slice(i, end - 1).join(' '), 'DATATYPE');
+                i = end - 2;
+            } else if (this.keyword(current)) {
+                this._label(current, 'KEYWORD');
+            } else if (tokens[i - 1].match(/^['"']$/)
+                    && tokens[i - 1] === tokens[i + 1]) {
+                this._label(current, 'STRING');
+            } else if (this.identifier(current)) {
+                this._label(current, 'IDENTIFIER');
+            } else if (this.number(current)) {
+                this._label(current, 'NUMBER');
+            } else if (this.operation(current)) {
+                var next = tokens[i + 1];
+                if (next === '=') {
+                    this._label(current + next, 'OPERATION_ASSIGNMENT');
+                    i++;
+                } else if (current === '+' && next === '+') {
+                    this._label(current + next, 'INCREMENT');
+                    i++;
+                } else if (current === '-' && next === '-') {
+                    this._label(current + next, 'DECREMEMENT');
+                    i++;
+                } else {
+                    this._label(current, 'OPERATION');
+                }
+            } else if (this.whitespace(current)) {
+                this._label(current, 'WHITESPACE');
+            } else if (current.match(/^[|&]$/)) {
+                var next = tokens[i + 1];
+                if (current === next) {
+                    this._label(current + next, 'LOGICAL_OPERATION');
+                    i++;
+                } else {
+                    this._label(current, 'OPERATION');
+                }
+            } else if (current.match(/^(<|>|=|!)$/)) {
+                var next = tokens[i + 1];
+                if (next === '=') {
+                    this._label(current + next, 'COMPARISON');
+                    i++;
+                } else if (current === '!') {
+                    this._label(current, 'NOT_OPERATION');
+                } else if (current === '<' || current === '>') {
+                    var end = i;
+                    while (!tokens[++end].match(/^[\n)?]$/));
+                    if (tokens[end] === '\n' && current === '<') {
+                        this._label(current, 'OPENING_ANGLE_BRACKET');
+                    } else if (tokens[end] === '\n' && current === '>') {
+                        this._label(current, 'CLOSING_ANGLE_BRACKET');
+                    } else {
+                        this._label(current, 'COMPARISON');
+                    }
+                } else if (current === '=') {
+                    this._label(current, 'ASSIGNMENT_OPERATION');
+                }
+            } else if (current === '.') {
+                var end = i;
+                while (!tokens[++end].match(/^[>|&)?]$/));
+                if (tokens[end] === '>') {
+                    var value = '';
+                    var token = this.tokens.pop();
+                    while (token.token !== '<') {
+                        value += token.token;
+                        token = this.tokens.pop();
+                    }
+                    this.tokens.push(token);
+                    value += tokens.slice(i, end).join('');
+                    i = end - 1;
+                    this._label(value, 'HEADER_FILE');
+                } else {
+                    this._label(current, 'DOT_OPERATION');
+                }
+            } else if (current.match(/^[,;]$/)) {
+                this._label(current, 'DELIMITER');
+            } else if (current === '"') {
+                this._label(current, 'DOUBLE_QUOTE');
+            } else if (current === '\'') {
+                this._label(current, 'SINGLE_QUOTE');
+            } else if (current === '(') {
+                this._label(current, 'OPENING_PARENTHESIS');
+            } else if (current === ')') {
+                this._label(current, 'CLOSING_PARENTHESIS');
+            } else if (current === '[') {
+                this._label(current, 'OPENING_BRACKET');
+            } else if (current === ']') {
+                this._label(current, 'CLOSING_BRACKET');
+            } else if (current === '{') {
+                this._label(current, 'OPENING_BRACES');
+            } else if (current === '}') {
+                this._label(current, 'CLOSING_BRACES');
+            } else if (current === '?') {
+                this._label(current, 'TERNARY_CONDITION');
+            } else if (current === ':') {
+                this._label(current, 'TERNARY_SELECTION');
+            } else {
+                this._label(current, 'UNCLASSIFIED');
+            }
+        }
+    }
+
+    TokenizedCode.prototype.letter = function(token) {
+        return token && token.match(/^[A-Za-z_]$/);
+    };
+
+    TokenizedCode.prototype.digit = function(token) {
+        return token && token.match(/^[0-9]$/);
+    };
+
+    TokenizedCode.prototype.number = function(token) {
+        return token && token.match(/^([0-9]*|(-|\+)[0-9]+)(\.[0-9]+)?$/);
+    };
+
+    TokenizedCode.prototype.alphanumeric = function(token) {
+        return token && token.match(/[A-Za-z0-9_]+/);
+    };
+
+    TokenizedCode.prototype.whitespace = function(token) {
+        return token && token.match(/^[ \n\t]$/);
+    };
+
+    TokenizedCode.prototype.datatype = function(token) {
+        var datatypes = ['char', 'signed char', 'unsigned char', 'short',
+            'short int', 'signed short', 'signed short int', 'unsigned short',
+            'unsigned short int', 'int', 'signed int', 'unsigned',
+            'unsigned int', 'long', 'long int', 'signed long',
+            'signed long int', 'unsigned long', 'unsigned long int',
+            'long long', 'long long int', 'signed long long',
+            'signed long long int', 'unsigned long long',
+            'unsigned long long int', 'float', 'double', 'long double', 'void'];
+        for (var i = 0; i < datatypes.length; i++) {
+            if (datatypes[i].indexOf(token) === 0
+                    && (datatypes[i].substring(token.length).charAt(0) === ' '
+                    || !datatypes[i].substring(token.length).charAt(0))) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    TokenizedCode.prototype.keyword = function(token) {
+        var keywords = ['break', 'case', 'const', 'continue', 'default', 'do',
+            'else', 'entry', 'extern', 'for', 'goto', 'if', 'register',
+            'return', 'sizeof', 'static', 'struct', 'switch', 'typedef',
+            'union', 'while'];
+        for (var i = 0; i < keywords.length; i++) {
+            if (keywords[i] === token) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    TokenizedCode.prototype.identifier = function(token) {
+        return !!token.match(/^[A-Za-z_][A-Za-z0-9_]*$/);
+    };
+
+    TokenizedCode.prototype.operation = function(token) {
+        return !!token.match(/^[%/*\+-]$/);
+    };
+
+    TokenizedCode.prototype._label = function(token, label) {
+        this.tokens.push({ token: token, label: label });
+    };
+
+
+    function tokenize(code) {
+        var result = new TokenizedCode(code);
+        return result.tokens;
+    }
+
+
+    return tokenize;
+});
