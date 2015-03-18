4fa60401850c9cf21c24d7e5ec6091aabb837084
Arnelle Balane <arnellebalane@gmail.com>
2015-03-18 22:49:31 +0800

    updated code tokenizer test specs

--- code-tokenizer/tests/code-tokenizer-tests.js
+++ code-tokenizer/tests/code-tokenizer-tests.js
@@ -21,4 +21,13 @@ describe('Code Tokenizer', function() {
         expect(tokens[6]).to.eql({ label: 'DATATYPE', token: 'int' });
         expect(tokens[10]).to.eql({ label: 'DATATYPE', token: 'void' });
     });
+
+    it('should recognize identifiers as tokens', function() {
+        var given = '#include<stdio.h>\nint main(void) {\nint a = 1, b = 2;\nif (1 > 0) {\nswitch (1) {\ncase 1: return 12; break;\ndefault: return 12;\n}\n} else {\nreturn 24;\n}\n}';
+        var tokens = tokenize(given);
+        expect(tokens[1]).to.eql({ label: 'IDENTIFIER', token: 'include' });
+        expect(tokens[8]).to.eql({ label: 'IDENTIFIER', token: 'main' });
+        expect(tokens[17]).to.eql({ label: 'IDENTIFIER', token: 'a' });
+        expect(tokens[24]).to.eql({ label: 'IDENTIFIER', token: 'b' });
+    });
 });
